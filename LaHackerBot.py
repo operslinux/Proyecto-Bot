d3fcf47993f339dfca64d8865c12843f96825f0f
Opers Linux <59351751+operslinux@users.noreply.github.com>
2021-03-15 21:30:24 +0000

    Update LaHackerBot.py
    Funcion agregada

--- LaHackerBot.py
+++ LaHackerBot.py
@@ -145,7 +145,7 @@ def main():
     # Create the Updater and pass it your bot's token.
     # Make sure to set use_context=True to use the new context based callbacks
     # Post version 12 this will no longer be necessary
-    updater = Updater("1642041332:AAGiTV4_LWC3jakhoLdXZFTdbFnvx-AQCuw", use_context=True)
+    updater = Updater("token", use_context=True)
 
     # Get the dispatcher to register handlers
     dp = updater.dispatcher
